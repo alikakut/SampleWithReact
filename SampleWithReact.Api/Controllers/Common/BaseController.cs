b379e73e74e1c9ed8a5e478f6923d331348dcef3
Ali Kakut <ali.kakut@affinitybox.com>
2023-04-24 12:10:35 +0300

    .

--- SampleWithReact.Api/Controllers/Common/BaseController.cs
+++ SampleWithReact.Api/Controllers/Common/BaseController.cs
@@ -1,50 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SampleWithReact.Api.Controllers.Common
 {
     [ApiController]
     public class BaseController : ControllerBase
     {
-        //protected IActionResult Problem(List<Error> errors)
-       // {
-            //if (errors.IsNullOrEmpty())
+        protected IActionResult Problem(List<Error> errors)
+        {
+            if (errors.IsNullOrEmpty())
+            {
+                return Problem();
+            }
+
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            return Problem(errors[0]);
+        }
+
+        private IActionResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            // If it is about credential just show that error and make status code 401
+            //if (error == Errors.Authentication.InvalidCredentials)
             //{
-            //    return Problem();
+            //    HttpContext.Items[HttpContextItemKeys.Errors] = new List<Error>() {error};
+            //    statusCode = StatusCodes.Status401Unauthorized;
+            //}
+            //else
+            //{
+            // HttpContext.Items[HttpContextItemKeys.Errors] = error;
             //}
 
-            //if (errors.All(error => error.Type == ErrorType.Validation))
-            //{
-            //    return ValidationProblem(errors);
-            //}
-
-            //return Problem(errors[0]);
-       // }
-
-        //private IActionResult Problem(Error error)
-        //{
-        //    var statusCode = error.Type switch
-        //    {
-        //        ErrorType.Conflict => StatusCodes.Status409Conflict,
-        //        ErrorType.Validation => StatusCodes.Status400BadRequest,
-        //        ErrorType.NotFound => StatusCodes.Status404NotFound,
-        //        _ => StatusCodes.Status500InternalServerError
-        //    };
-
-        //    // If it is about credential just show that error and make status code 401
-        //    //if (error == Errors.Authentication.InvalidCredentials)
-        //    //{
-        //    //    HttpContext.Items[HttpContextItemKeys.Errors] = new List<Error>() {error};
-        //    //    statusCode = StatusCodes.Status401Unauthorized;
-        //    //}
-        //    //else
-        //    //{
-        //   // HttpContext.Items[HttpContextItemKeys.Errors] = error;
-        //    //}
-
-        //    return Problem(statusCode: statusCode, title: error.Description);
-        //}
+            return Problem(statusCode: statusCode, title: error.Description);
+        }
 
         private IActionResult ValidationProblem(List<Error> errors)
         {
@@ -52,7 +53,7 @@ namespace SampleWithReact.Api.Controllers.Common
 
             foreach (var error in errors)
             {
-                modelStateDictionary.AddModelError(error.ToString(),error.ToString());//düzelt
+                modelStateDictionary.AddModelError(error.ToString(),error.ToString());
             }
 
             return ValidationProblem(modelStateDictionary);
