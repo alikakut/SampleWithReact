5ab4ba363f47956406300d689c73936dc9d03725
Ali Kakut <ali.kakut@affinitybox.com>
2023-04-24 12:28:36 +0300

    StudentController

--- SampleWithReact.Api/Controllers/StudentController.cs
+++ SampleWithReact.Api/Controllers/StudentController.cs
@@ -1,15 +1,19 @@
 ﻿using AutoMapper;
+using ErrorOr;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 using SampleWithReact.Api.Contracts.Student;
+using SampleWithReact.Api.Controllers.Common;
+using SampleWithReact.Application.Students.Commands.CreateStudents;
+using SampleWithReact.Application.Students.Queries.GetByIdStudents;
 
 namespace SampleWithReact.Api.Controllers
 {
     [Route("api/[controller]")]
     [AllowAnonymous]
-    public class StudentController : Controller
+    public class StudentController : BaseController
     {
         private readonly ISender _mediator;
         private readonly IMapper _mapper;
@@ -20,30 +24,30 @@ namespace SampleWithReact.Api.Controllers
             _mapper = mapper;
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> Get([FromQuery] StudentPagedRequest request)
-        //{
-        //    var query = _mapper.Map<StudentQuery>(request);
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] StudentPagedRequest request)
+        {
+            var query = _mapper.Map<StudentQuery>(request);
 
-        //    ErrorOr<StudentQueryResult> queryResult = await _mediator.Send(query);
+            ErrorOr<StudentQueryResult> queryResult = await _mediator.Send(query);
 
-        //    return queryResult.Match(
-        //        result => Ok(_mapper.Map<StudentPagedResponse>(result)),
-        //        Problem);
-        //}
+            return queryResult.Match(
+                result => Ok(_mapper.Map<StudentPagedResponse>(result)),
+                Problem);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Create(CreateStudentRequest request)
-        //{
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateStudentRequest request)
+        {
 
-        //    var command = _mapper.Map<CreateStudentCommand>(request);
+            var command = _mapper.Map<CreateStudentCommand>(request);
 
-        //    var createResult = await _mediator.Send(command);
+            var createResult = await _mediator.Send(command);
 
-        //    return createResult.Match(
-        //        result => Ok(_mapper.Map<StudentResponse>(result)),
-        //        Problem);
-        //}
+            return createResult.Match(
+                result => Ok(_mapper.Map<StudentResponse>(result)),
+                Problem);
+        }
     }
 }
 
