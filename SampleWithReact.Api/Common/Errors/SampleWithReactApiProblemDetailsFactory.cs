2b325b4287526fbd15d0f64228e81d96ce91129d
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2023-04-24 12:55:50 +0300

    .

--- SampleWithReact.Api/Common/Errors/SampleWithReactApiProblemDetailsFactory.cs
+++ SampleWithReact.Api/Common/Errors/SampleWithReactApiProblemDetailsFactory.cs
@@ -1,6 +1,110 @@
-﻿namespace SampleWithReact.Api.Common.Errors
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace SampleWithReact.Api.Common.Errors
 {
-    public class SampleWithReactApiProblemDetailsFactory
+    public class SampleWithReactApiProblemDetailsFactory: ProblemDetailsFactory
     {
+        private readonly ApiBehaviorOptions _options;
+        private readonly Action<ProblemDetailsContext>? _configure;
+
+        public SampleWithReactApiProblemDetailsFactory(
+        IOptions<ApiBehaviorOptions> options,
+        IOptions<ProblemDetailsOptions>? problemDetailsOptions = null)
+        {
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+            _configure = problemDetailsOptions?.Value?.CustomizeProblemDetails;
+        }
+
+        public override ProblemDetails CreateProblemDetails(
+            HttpContext httpContext,
+            int? statusCode = null,
+            string? title = null,
+            string? type = null,
+            string? detail = null,
+            string? instance = null)
+        {
+            statusCode ??= 500;
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = title,
+                Type = type,
+                Detail = detail,
+                Instance = instance,
+            };
+
+            ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+            return problemDetails;
+        }
+
+        public override ValidationProblemDetails CreateValidationProblemDetails(
+            HttpContext httpContext,
+            ModelStateDictionary modelStateDictionary,
+            int? statusCode = null,
+            string? title = null,
+            string? type = null,
+            string? detail = null,
+            string? instance = null)
+        {
+            if (modelStateDictionary == null)
+            {
+                throw new ArgumentNullException(nameof(modelStateDictionary));
+            }
+
+            statusCode ??= 400;
+
+            var problemDetails = new ValidationProblemDetails(modelStateDictionary)
+            {
+                Status = statusCode,
+                Type = type,
+                Detail = detail,
+                Instance = instance,
+            };
+
+            if (title != null)
+            {
+                // For validation problem details, don't overwrite the default title with null.
+                problemDetails.Title = title;
+            }
+
+            ApplyProblemDetailsDefaults(httpContext, problemDetails, statusCode.Value);
+
+            return problemDetails;
+        }
+
+        private void ApplyProblemDetailsDefaults(HttpContext httpContext, ProblemDetails problemDetails, int statusCode)
+        {
+            problemDetails.Status ??= statusCode;
+
+            if (_options.ClientErrorMapping.TryGetValue(statusCode, out var clientErrorData))
+            {
+                problemDetails.Title ??= clientErrorData.Title;
+                problemDetails.Type ??= clientErrorData.Link;
+            }
+
+            var traceId = Activity.Current?.Id ?? httpContext?.TraceIdentifier;
+            if (traceId != null)
+            {
+                problemDetails.Extensions["traceId"] = traceId;
+            }
+
+            problemDetails.Extensions.Add("help", "info@help.com");
+
+            if (httpContext?.Items[HttpContextItemKeys.Errors] is List<Error> errors)
+            {
+                problemDetails.Extensions.Add("errorCodes", errors.Select(e => e.Code));
+            }
+
+            // Commented to get rid of IDE error!
+            //_configure?.Invoke(new() { HttpContext = httpContext!, ProblemDetails = problemDetails });
+        }
+
+
+
     }
-}
