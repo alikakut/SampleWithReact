7f5a45999264d7bea2471ed8c282e66c705eb3a9
Ali Kakut <ali.kakut@affinitybox.com>
2023-04-25 16:20:02 +0300

    .

--- SampleWithReact.Api/Contracts/Lecturers/LecturerPagedResponse.cs
+++ SampleWithReact.Api/Contracts/Lecturers/LecturerPagedResponse.cs
@@ -1,4 +1,8 @@
 ﻿namespace SampleWithReact.Api.Contracts.Lecturers
 {
-    public record LecturerPagedResponse();
+    public record LecturerPagedResponse(int CurrentPage,
+      int PageSize,
+      int TotalPageCount,
+      int TotalRowCount,
+      List<LecturerPagedResponse> Data);
 }
