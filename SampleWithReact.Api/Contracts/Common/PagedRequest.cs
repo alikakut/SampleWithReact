a0d4847bf7ffb6347b8492476dd2932db0d278d1
Ali Kakut <ali.kakut@affinitybox.com>
2023-04-25 15:49:36 +0300

    .

--- SampleWithReact.Api/Contracts/Common/PagedRequest.cs
+++ SampleWithReact.Api/Contracts/Common/PagedRequest.cs
@@ -3,6 +3,6 @@
     public record PagedRequest
     {
         public int Page { get; set; }   
-        public int size { get; set; }  
+        public int Size { get; set; }  
     }
 }
--- SampleWithReact.Domain/Entities/LecturerEntity.cs
+++ SampleWithReact.Domain/Entities/LecturerEntity.cs
@@ -18,6 +18,6 @@ namespace SampleWithReact.Domain.Entities
         public string LastName { get; set; }
 
         [Column("is_active")]
-        public byte IsActive { get; set; }
+        public bool IsActive { get; set; }
     }
 }
